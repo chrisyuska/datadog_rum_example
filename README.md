e541646dc878d5c770ba31ddf45da3e4a084f176
Chris Yuska <chrisyuska@gmail.com>
2024-06-25 20:50:10 -0400

    Copy-paste Datadog snippet into application.js

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # README
 
+This is a sample application for reproducing the failure to inject Datadog
+tracing headers into Turbo requests, starting at the Turbo 8.0.0-beta.2 release.
+Downgrading to Turbo 8.0.0-beta.1 injects Datadog tracing headers as expected.
+This is meant to be an as-barebones-example-as-possible for reproducing the
+issue and should not be used for any other purpose.
+
 Steps to reproduce:
 
 1. `rails new datadog_rum_example`
 2. `./bin/importmap pin @datadog/browser-rum`
 3. `rails g controller main index one`
   - Add nav links
+4. Copy-paste Datadog snippet into application.js
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,20 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
+// import "controllers"
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+  applicationId: '<APPLICATION_ID>',
+  clientToken: '<CLIENT_TOKEN>',
+  site: "datadoghq.com",
+  service: "datadog-rum-example",
+  env: "development",
+  version: "0.0.1",
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 20,
+  trackUserInteractions: true,
+  trackResources: true,
+  trackLongTasks: true,
+  defaultPrivacyLevel: "allow",
+  allowedTracingUrls: ["http://localhost:3000"]
+});
